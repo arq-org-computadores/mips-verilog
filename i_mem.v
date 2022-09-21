7e3d041f0cfb66549645dfbd142a60a0723f4668
Moésio Filho <moesio.f@gmail.com>
2022-09-20 23:10:56 -0300

    fix: i_mem
    - Removed double declaration;

--- i_mem.v
+++ i_mem.v
@@ -1,19 +1,16 @@
 module i_mem(
-	input		[31:0]	address ,
-	output reg		[31:0]	i_out
+        input		[31:0]	address ,
+        output reg		[31:0]	i_out
     );
 
-	input [31:0] address;
-	output reg [31:0] i_out;
-	
-	reg [31:0] instructions [255:0];
-	
-	initial begin 
-		$readmemb("instruction.list", instructions);
-	end
-	
-	always @ (address) begin
-		i_out = instructions[address];
-	end
+    reg [31:0] instructions [255:0];
+
+    initial begin
+        $readmemb("instruction.list", instructions);
+    end
+
+    always @ (address) begin
+        i_out = instructions[address];
+    end
 
 endmodule
